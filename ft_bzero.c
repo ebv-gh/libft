c0ad1a4731e4fcbe81480b4332f0888292f95497
Eugeniu Botnaru <eubotnar@e1z3r9p7.42.us.org>
2018-12-04 11:09:24 -0800

    ft_bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,27 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: eubotnar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/30 10:24:03 by eubotnar          #+#    #+#             */
-/*   Updated: 2018/12/03 13:49:14 by eubotnar         ###   ########.fr       */
+/*   Created: 2018/12/03 13:57:06 by eubotnar          #+#    #+#             */
+/*   Updated: 2018/12/03 17:20:47 by eubotnar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include <string.h>
 
-void	*ft_memset(void *s, int c, size_t len)
+void	ft_bzero(void *s, size_t n)
 {
-	unsigned char	*dst;
-
-	dst = (unsigned char*)s;
-	while (len > 0)
-	{
-		*dst = (unsigned char)c;
-		dst++;
-		len--;
-	}
-	return (s);
+	ft_memset(s, 0, n);
 }
+//void	ft_bzero(void *s, size_t n)
+//{
+//	unsigned char *ptr;
+//
+//	ptr = (unsigned char*)s;
+//	while (n > 0)
+//	{
+//		*ptr = '\0';
+//		ptr++;
+//		n--;
+//	}
+//}
