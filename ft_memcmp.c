20fde5078e251f9d18cd0deefde9445041eb84ec
Eugeniu Botnaru <eubotnar@e1z3r9p7.42.us.org>
2018-12-05 16:20:38 -0800

    create ft_memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eubotnar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/05 15:34:21 by eubotnar          #+#    #+#             */
+/*   Updated: 2018/12/05 16:19:58 by eubotnar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+int		ft_memcmp(const void* lhs, const void* rhs, size_t count)
+{
+	unsigned char uc1;
+	unsigned char uc2;
+
+	while (count--)
+	{
+		uc1 = * (unsigned char *)lhs;
+		uc2 = * (unsigned char *)rhs;
+		if (uc1 != uc2)
+			return (uc1 - uc2);
+		lhs++;
+		rhs++;
+	}
+	return (0);
+}
