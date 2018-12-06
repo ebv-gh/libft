0faf4caeb58d2f05d4e69773b1f91fb49c75b551
Eugeniu Botnaru <eubotnar@e1z3r9p7.42.us.org>
2018-12-05 17:07:03 -0800

    create ft_strlen

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,16 +6,18 @@
 /*   By: eubotnar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/26 13:31:09 by eubotnar          #+#    #+#             */
-/*   Updated: 2018/09/26 13:33:04 by eubotnar         ###   ########.fr       */
+/*   Updated: 2018/12/05 17:06:42 by eubotnar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_strlen(char *str)
-{
-	int		i;
+#include <string.h>
 
-	i = 0;
-	while (str[i])
-		i++;
-	return i;
+size_t		ft_strlen(const char *str)
+{
+	const char *p;
+
+	p = str;
+	while (*p)
+		p++;
+	return (p - str);
 }
