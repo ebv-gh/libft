88d3b97bbed838f9c79db0c4c80f27c03f09843f
Eugeniu Botnaru <enbotnaru@gmail.com>
2018-12-11 14:43:27 -0800

    update ft_strcat.c

--- ft_strcat.c
+++ ft_strcat.c
@@ -6,15 +6,11 @@
 /*   By: eubotnar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/07 15:08:14 by eubotnar          #+#    #+#             */
-/*   Updated: 2018/12/07 15:22:33 by eubotnar         ###   ########.fr       */
+/*   Updated: 2018/12/11 14:42:46 by eubotnar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-/* The strncat() function appends not more than n characters from s2, and then
- * adds a terminating `\0'.
- * */
 
 char	*ft_strcat(char *dst, const char *src)
 {
