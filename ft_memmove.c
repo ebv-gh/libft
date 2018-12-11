f59acb90dcc223c6daba1af63dd2eddb28c40966
Eugeniu Botnaru <enbotnaru@gmail.com>
2018-12-11 13:52:47 -0800

    update ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,16 +6,11 @@
 /*   By: eubotnar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/05 12:13:32 by eubotnar          #+#    #+#             */
-/*   Updated: 2018/12/05 15:11:34 by eubotnar         ###   ########.fr       */
+/*   Updated: 2018/12/11 13:39:03 by eubotnar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-
-/* What memmove() actually does is copy the bytes from src to dest, and it
- * copies forward if dest < src (which is essentially the same thing as memcpy),
- * and backwards otherwise.*/
-/* find out if have to add 1 to len in else */
+#include "libft.h"
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
