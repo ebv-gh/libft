e4bc81bef0a0e3c3bdf9c6cbae6c5654879d8233
Eugeniu Botnaru <enbotnaru@gmail.com>
2019-01-07 19:12:53 -0800

    create ft_strmapi

---
+++ str/ft_strmapi.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eubotnar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/07 19:06:16 by eubotnar          #+#    #+#             */
+/*   Updated: 2019/01/07 19:12:36 by eubotnar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	char			*res;
+
+	if (s != NULL)
+	{
+		if (!(res = ft_memalloc(ft_strlen(s) + 1)))
+			return (NULL);
+		i = 0;
+		while (s[i])
+		{
+			res[i] = f(i, s[i]);
+			i++;
+		}
+		res[i] = '\0';
+		return (res);
+	}
+	return (NULL);
+}
