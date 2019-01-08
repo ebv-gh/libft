8c85dcbec202072244267825ee31757ea36722ff
Eugeniu Botnaru <enbotnaru@gmail.com>
2019-01-07 18:34:14 -0800

    create ft_lstnew

---
+++ ft_lstnew.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eubotnar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/07 18:30:15 by eubotnar          #+#    #+#             */
+/*   Updated: 2019/01/07 18:33:32 by eubotnar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list		*ft_lstnew(void const *content, size_t content_size)
+{
+	t_list *list;
+
+	if (!(list = ft_memalloc(sizeof(t_list))))
+		return (NULL);
+	if (content == NULL)
+	{
+		list->content = NULL;
+		list->content_size = 0;
+	}
+	else
+	{
+		if (!(list->content = malloc(content_size)))
+			return (NULL);
+		ft_memcpy(list->content, content, content_size);
+		list->content_size = content_size;
+	}
+	list->next = NULL;
+	return (list);
+}
