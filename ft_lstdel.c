b7722a3453659ebde14fc17c6aadc17195c04e3a
Eugeniu Botnaru <enbotnaru@gmail.com>
2019-01-07 18:11:43 -0800

    create ft_lstdel

---
+++ ft_lstdel.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdel.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eubotnar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/07 18:09:58 by eubotnar          #+#    #+#             */
+/*   Updated: 2019/01/07 18:11:27 by eubotnar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void    ft_lstdel(t_list **alst, void (*del)(void *, size_t))
+{
+	while (*alst)
+	{
+		del((*alst)->content, ((*alst)->content_size));
+		free(*alst);
+		*alst = (*alst)->next;
+	}
+	*alst = NULL;
+}
