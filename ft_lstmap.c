7853639998114a4dad0780cefb6b53277f8d227a
Eugeniu Botnaru <enbotnaru@gmail.com>
2019-01-07 18:27:51 -0800

    create ft_lstmap

---
+++ ft_lstmap.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eubotnar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/07 18:16:42 by eubotnar          #+#    #+#             */
+/*   Updated: 2019/01/07 18:27:27 by eubotnar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list		*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
+{
+	t_list *result;
+	t_list *head;
+	t_list *elem;
+
+	if (!lst || !f)
+		return (NULL);
+	elem = f(lst);
+	if (!(result = ft_lstnew(elem->content, elem->content_size)))
+		return (NULL);
+	lst = lst->next;
+	head = result;
+	while (lst)
+	{
+		elem = f(lst);
+		if (!(result->next = ft_lstnew(elem->content, elem->content_size)))
+			return (NULL);
+		result = result->next;
+		lst = lst->next;
+	}
+	return (head);
+}
