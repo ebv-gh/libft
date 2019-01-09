b4fc4100b82a086934bb236d11a304e2e6139f85
Eugeniu Botnaru <enbotnaru@gmail.com>
2019-01-09 11:49:01 -0800

    fix ft_lstdel.c

--- srcs/lst/ft_lstdel.c
+++ srcs/lst/ft_lstdel.c
@@ -6,13 +6,13 @@
 /*   By: eubotnar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/07 18:09:58 by eubotnar          #+#    #+#             */
-/*   Updated: 2019/01/07 18:11:27 by eubotnar         ###   ########.fr       */
+/*   Updated: 2019/01/08 15:44:26 by eubotnar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    ft_lstdel(t_list **alst, void (*del)(void *, size_t))
+void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 {
 	while (*alst)
 	{
