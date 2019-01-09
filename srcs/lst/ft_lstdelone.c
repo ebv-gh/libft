3aeefd2c8f7d116107ea44e44c3ce6a43e55cdcd
Eugeniu Botnaru <enbotnaru@gmail.com>
2019-01-09 11:49:27 -0800

    fix ft_lstdelone

--- srcs/lst/ft_lstdelone.c
+++ srcs/lst/ft_lstdelone.c
@@ -6,13 +6,13 @@
 /*   By: eubotnar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/07 18:12:10 by eubotnar          #+#    #+#             */
-/*   Updated: 2019/01/07 18:13:46 by eubotnar         ###   ########.fr       */
+/*   Updated: 2019/01/08 15:43:53 by eubotnar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
+void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
 {
 	del((*alst)->content, (*alst)->content_size);
 	free(*alst);
